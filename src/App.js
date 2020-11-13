6864f7b0d30d07e377e940c4fe04a7ec8648c9f4
hdriqi <riqi.asdf@gmail.com>
2020-11-13 14:27:42 +0700

    add skynet-js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
+import { genKeyPairAndSeed } from 'skynet-js'
 import './styles/tailwind.css'
 
 function App() {
+	const x = genKeyPairAndSeed()
+	console.log(x)
+
 	return (
 		<div className="flex items-center justify-center">
 			<div className="text-red-900 text-3xl">HELLO WORLD</div>

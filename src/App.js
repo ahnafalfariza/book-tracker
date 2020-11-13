32cd2e31f1dabe08ea4c37e35f857da11872b590
hdriqi <riqi.asdf@gmail.com>
2020-11-13 14:33:53 +0700

    add bip39

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import { genKeyPairAndSeed } from 'skynet-js'
+import { genKeyPairFromSeed } from 'skynet-js'
+import { generateMnemonic } from 'bip39'
 import './styles/tailwind.css'
 
 function App() {
-	const x = genKeyPairAndSeed()
-	console.log(x)
+	// generate new mnemonic with length 256
+	// const mnemonic = generateMnemonic(256)
+	// console.log(mnemonic)
+
+	const mnemonic = `wait eternal sphere excuse lift ozone brother curtain imitate chalk pear sound impulse badge kind vibrant arena regret broken ghost error amazing wild welcome`
+
+	const y = genKeyPairFromSeed(mnemonic)
+	console.log(y)
 
 	return (
 		<div className="flex items-center justify-center">

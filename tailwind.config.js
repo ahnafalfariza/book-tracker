0d05dd2f1fe5fba80ccd051fa6a667218f1b889e
hdriqi <riqi.asdf@gmail.com>
2020-11-16 12:47:38 +0700

    update color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,12 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'primary-color': '#4F00FF',
-        'dark-primary-900': '#18161D',
-        'dark-primary-800': '#1C1924',
-        'dark-primary-700': '#201B2B',
-        'dark-primary-600': '#241D32',
-        'dark-primary-500': '#261F36',
+        'primary-color': '#AD88FF',
+        'dark-primary-900': '#1A181E',
+        'dark-primary-800': '#1E1B25',
+        'dark-primary-700': '#231F2C',
+        'dark-primary-600': '#282333',
+        'dark-primary-500': '#2B2538',
       },
     },
   },

b619186500794057d2d7e238ffdc6f40b60ef96c
Ahnaf Alfariza <ahnafalfariza@outlook.com>
2020-11-16 14:24:04 +0700

    finish explore feature 1. add manual book 2. modal for: book, choose library, and add manual

--- src/pages/explore.js
+++ src/pages/explore.js
@@ -1,53 +1,85 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
-import Modal from '../components/Modal'
 import useStore from '../store'
 import { addToLibrary, getLibrary } from '../utils/skynet'
+import ChooseLibraryModal from '../components/ChooseLibraryModal'
+import BookModal from '../components/BookModal'
+import ManualAddModal from '../components/ManualAddModal'
 
 const Explore = () => {
   const [searchResult, setSearchResult] = useState(null)
-  const [showDetail, setShowDetail] = useState(false)
+  const [showModal, setshowModal] = useState(false)
   const [detailBook, setDetailBook] = useState(null)
-  const [choosenLibrary, setChoosenLibrary] = useState(undefined)
 
   const [isLoading, setIsLoading] = useState(false)
-
   const { booksData, setBooksData } = useStore((state) => state)
+
+  useEffect(() => {
+    getLibrary().then((res) => setBooksData(res))
+  }, [setBooksData])
 
   const onSearch = (event) => {
     const query = event.target.value
-    fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}`)
+    fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}&langRestrict=en&maxResults=20`)
       .then((res) => res.json())
-      .then((data) => setSearchResult(data.items.map((res) => res.volumeInfo)))
+      .then((data) => setSearchResult(data.items))
       .catch((err) => console.log(err))
   }
 
-  const chooseLibrary = (event) => {
-    setChoosenLibrary(event.target.value)
-  }
-
-  const onPressBook = (bookData) => {
-    setShowDetail(true)
+  const onPressAdd = (bookData) => {
+    setshowModal('add')
     setDetailBook(bookData)
   }
 
-  const onCloseDetail = () => {
-    setShowDetail(false)
-    setDetailBook(null)
-    setChoosenLibrary(null)
+  const onPressBook = (bookData) => {
+    setshowModal('detail')
+    setDetailBook(bookData)
   }
 
-  const addBook = async () => {
-    const newData = booksData
-    newData.push({ ...detailBook, libraryType: choosenLibrary })
+  const onCloseModal = () => {
+    setshowModal(false)
+    setDetailBook(null)
+  }
 
+  const convertMetaData = (bookData) => {
+    return {
+      id: bookData.id,
+      title: bookData.volumeInfo.title,
+      authors: bookData.volumeInfo.authors,
+      publisher: bookData.volumeInfo.publisher,
+      publishedDate: bookData.volumeInfo.publishedDate,
+      isbn: bookData.volumeInfo.industryIdentifiers,
+      description: bookData.volumeInfo.description,
+      pageCount: bookData.volumeInfo.pageCount,
+      categories: bookData.volumeInfo.categories,
+      imageLinks: bookData.volumeInfo.imageLinks,
+    }
+  }
+
+  const addBook = async (libraryType = 'readingList', book = detailBook, convert = true) => {
+    const newData = booksData
+    const newBook = convert
+      ? {
+          ...convertMetaData(book),
+          libraryType: libraryType,
+        }
+      : {
+          ...book,
+          libraryType: libraryType,
+        }
+
+    newData.push(newBook)
     setIsLoading(true)
     await addToLibrary(newData)
     setBooksData(newData)
     setIsLoading(false)
 
     console.log(await getLibrary())
+  }
+
+  const checkIfBookInLibrary = (id) => {
+    return booksData.some((bookData) => bookData.id === id)
   }
 
   return (
@@ -59,54 +91,46 @@ const Explore = () => {
             <Link to="/explore">Explore</Link>
           </div>
           <div>
-            <Link to="/library">Library</Link>
+            <Link to="/">Library</Link>
           </div>
         </div>
         <div className="w-full md:w-4/5">
           <form>
-            <input
-              type="text"
-              onChange={onSearch}
-              className="border-2 border-black mb-3"
-            />
+            <input type="text" onChange={onSearch} className="border-2 border-black mb-3" />
           </form>
+          <button onClick={() => setshowModal('manualAdd')}>Manual add</button>
           {searchResult &&
             searchResult.map((result) => (
-              <div className="flex flex-row justify-between">
-                <p onClick={() => console.log(result.title)}>{result.title}</p>
-                <button
-                  className="px-1 right-0"
-                  onClick={() => onPressBook(result)}
-                >
-                  Add to Library
-                </button>
+              <div key={result.id} className="flex flex-col justify-between my-4">
+                <div>
+                  <button onClick={() => onPressBook(result)}>
+                    {result.volumeInfo.imageLinks && (
+                      <img src={result.volumeInfo.imageLinks.thumbnail} alt="Book Cover" />
+                    )}
+                    {result.volumeInfo.title}
+                  </button>
+                </div>
+                {result.volumeInfo.authors && <h1>by. {result.volumeInfo.authors.join(',')}</h1>}
+                <div>
+                  <button onClick={() => onPressAdd(result)}>
+                    {checkIfBookInLibrary(result.id) ? 'Added' : 'Add to Library'}
+                  </button>
+                </div>
               </div>
             ))}
-          {showDetail && (
-            <Modal close={onCloseDetail}>
-              <div className="max-w-xl w-full px-4 py-2 bg-gray-100 m-auto rounded-md">
-                <h1>{detailBook.title}</h1>
-                <h2>by {detailBook.authors}</h2>
-                <form onSubmit={!isLoading ? addBook : null}>
-                  <div className="flex flex-col">
-                    <label>
-                      Choose your library
-                      <select value={choosenLibrary} onChange={chooseLibrary}>
-                        <option disabled selected value>
-                          {'  -- select an option --  '}
-                        </option>
-                        <option value="readingList">Reading List</option>
-                        <option value="readingNow">Reading Now</option>
-                        <option value="finished">Finished</option>
-                      </select>
-                    </label>
-                    <button type="submit">
-                      {isLoading ? 'Loading' : 'Add Book'}
-                    </button>
-                  </div>
-                </form>
-              </div>
-            </Modal>
+          {showModal === 'add' && (
+            <ChooseLibraryModal
+              isLoading={isLoading}
+              onClose={onCloseModal}
+              bookTitle={detailBook.volumeInfo.title}
+              authors={detailBook.volumeInfo.authors}
+              header={'Add Book'}
+              onPress={addBook}
+            />
+          )}
+          {showModal === 'detail' && <BookModal bookData={convertMetaData(detailBook)} onClose={onCloseModal} />}
+          {showModal === 'manualAdd' && (
+            <ManualAddModal onClose={onCloseModal} onPressAdd={addBook} isLoading={isLoading} />
           )}
         </div>
       </div>

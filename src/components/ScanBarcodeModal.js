95b1ef88368d3bbe39cd86d12798ca72f9d61a9f
Ahnaf Alfariza <ahnafalfariza@outlook.com>
2020-11-18 21:19:36 +0700

    choose input camera

--- src/components/ScanBarcodeModal.js
+++ src/components/ScanBarcodeModal.js
@@ -1,14 +1,17 @@
-import React, { useEffect } from 'react'
-import Modal from './Modal'
-
+import React, { useEffect, useState } from 'react'
 import { BrowserBarcodeReader } from '@zxing/library'
 
+import Modal from './Modal'
+
 const ScanBarcodeModal = ({ onFinish, onClose }) => {
+  const [selectedDeviceId, setSelectedDeviceId] = useState(null)
+  const [listInputDevice, setListInputDevice] = useState([])
+
   useEffect(() => {
     const codeReader = new BrowserBarcodeReader()
     codeReader.getVideoInputDevices().then((videoInputDevices) => {
       console.log(videoInputDevices)
-      let selectedDeviceId = videoInputDevices[0].deviceId
+      setListInputDevice(videoInputDevices)
       codeReader
         .decodeOnceFromVideoDevice(selectedDeviceId, 'video')
         .then((result) => {
@@ -20,12 +23,22 @@ const ScanBarcodeModal = ({ onFinish, onClose }) => {
         })
     })
     return () => codeReader.reset()
-  }, [onFinish])
+  }, [onFinish, selectedDeviceId])
 
   return (
     <Modal close={onClose}>
       <div className="max-w-xl w-full px-4 py-2 bg-black m-auto rounded-md">
         <h1>Scan Barcode</h1>
+        <div>
+          <label>Change video source:</label>
+          <select value={selectedDeviceId} onChange={(event) => setSelectedDeviceId(event.target.value)}>
+            {listInputDevice.map((cam) => (
+              <option key={cam.deviceId} value={cam.deviceId}>
+                {cam.label}
+              </option>
+            ))}
+          </select>
+        </div>
         <video id="video" width="900" height="900"></video>
       </div>
     </Modal>
